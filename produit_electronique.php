e3018d6fe94fe673c8a735ed5973c703c54c567f
nisniss <mhd.anissa7@gmail.com>
2026-03-13 13:59:10 +0100

    Add files via upload

---
+++ produit_electronique.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Produits : Viandes</title>
+    <link rel="stylesheet" href="style.css"> </head>
+<body>
+
+    <div class="main-page">
+        <div class="content-box-produits">
+            
+            <h1 class="titre-produits">Famille : Électroniques </h1>
+            <h2 class="sous-titre-produits">Sélectionnez les articles à ajouter au panier</h2>
+
+            <div class="produits-list">
+                
+                <div class="produit-card">
+                    
+                    <img src="oralb.webp" 
+                         alt="Brosse à dents Oral-B" 
+                         class="produit-image">
+                    
+                    <div class="produit-details">
+                        <h3 class="produit-name">Brosse à dents Oral-B</h3>
+                        <div class="produit-prix">
+                            **53,30 €**
+                        </div>
+                        
+                        <button class="btn btn-add-to-cart">Ajouter au panier</button>
+                    </div>
+                </div>
+        
+                <div class="produit-card">
+                    
+                    <img src="apta.jpg" 
+                         alt="Diffuseur anti-moustiques Apta" 
+                         class="produit-image">
+                    
+                    <div class="produit-details">
+                        <h3 class="produit-name">Diffuseur anti-moustiques Apta</h3>
+                        <div class="produit-prix">
+                            **6,80 €**
+                        </div>
+                        
+                        <button class="btn btn-add-to-cart">Ajouter au panier</button>
+                    </div>
+                </div>
+
+                <div class="produit-card">
+                    
+                    <img src="cable.webp" 
+                         alt="Câble rallonge 10M Workzone" 
+                         class="produit-image">
+                    
+                    <div class="produit-details">
+                        <h3 class="produit-name">Câble rallonge 10M Workzone</h3>
+                        <div class="produit-prix">
+                            **5,71 €**
+                        </div>
+                        
+                        <button class="btn btn-add-to-cart">Ajouter au panier</button>
+                    </div>
+                </div>
+
+                <div class="produit-card">
+                    
+                    <img src="hachoir.webp" 
+                         alt="Hachoir électrique sans fil" 
+                         class="produit-image">
+                    
+                    <div class="produit-details">
+                        <h3 class="produit-name">Hachoir électrique sans fil Ambiano</h3>
+                        <div class="produit-prix">
+                            **12,99 €**
+                        </div>
+                        
+                        <button class="btn btn-add-to-cart">Ajouter au panier</button>
+                    </div>
+                </div>
+
+                <div class="produit-card">
+                    
+                    <img src="souffleur.webp" 
+                         alt="Souffleur axial électrique" 
+                         class="produit-image">
+                    
+                    <div class="produit-details">
+                        <h3 class="produit-name">Souffleur axial électrique</h3>
+                        <div class="produit-prix">
+                            **64,92 €**
+                        </div>
+                        
+                        <button class="btn btn-add-to-cart">Ajouter au panier</button>
+                    </div>
+                </div>
+
+                <div class="produit-card">
+                    
+                    <img src="moulin.webp" 
+                         alt="Moulin à café électrique" 
+                         class="produit-image">
+                    
+                    <div class="produit-details">
+                        <h3 class="produit-name">Moulin à café électrique</h3>
+                        <div class="produit-prix">
+                            **29,55 €**
+                        </div>
+                        
+                        <button class="btn btn-add-to-cart">Ajouter au panier</button>
+                    </div>
+                </div>
+
+
+            </div>
+
+            <div class="zone-boutons-retour">
+                <a href="passer_commande.php" class="btn btn-secondary">Retour aux familles</a>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
